a8dece62054635c11d01c965451dc8d5ff7c1f25
kamran <khosravi42@gmail.com>
2022-09-10 10:27:04 -0500

    styles are being added

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,19 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post is op</h1>;
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
+    <Layout>
+      <>
+        <Head>
+          <title>Welcome to this page</title>
+        </Head>
+        <h1>First Post is op</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </>
+    </Layout>
   );
 }
---
+++ pages/posts/second-post.js
@@ -0,0 +1,7 @@
+export default function SecondPost() {
+  return (
+    <>
+      <h1>Second Post is op</h1>;
+    </>
+  );
+}
