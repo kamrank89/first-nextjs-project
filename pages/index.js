89af5ac973106f273b1ebbe13844e8176d1bd1ed
kamran <khosravi42@gmail.com>
2022-09-19 10:36:46 -0500

    fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
@@ -8,8 +9,13 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Hello Hello I am kamarn]</p>
+      <section>
+        <p className="underline text-green-400">
+          Hello Hello I am bagh baghusdasdsad
+        </p>
+        <Link href="/posts/first-post" className="underline ">
+          lets go to the first post
+        </Link>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,27 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <Layout>
       <>
         <Head>
-          <title>Welcome to this page</title>
+          <title>Welcome to first page</title>
         </Head>
-        <h1>First Post is op</h1>
-        <h2>
-          <Link href="/">Back to home</Link>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <h1 className="underline text-red-300">First Post is op</h1>
+        <h2 className="underline text-red-400">
+          <Link href="/">
+            <a className="text-blue-400">Back to home!!!!</a>
+          </Link>
         </h2>
       </>
     </Layout>
